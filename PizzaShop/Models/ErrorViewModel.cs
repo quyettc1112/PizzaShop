baf9e605db6e637e0b506c7403299d0acc2a73bf
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-02-02 00:15:28 +0700

    Add project files.

---
+++ PizzaShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PizzaShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

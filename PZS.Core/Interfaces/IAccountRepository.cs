6ea1519e1642446f68c871dadd936061a47887fd
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-02-02 00:25:10 +0700

    Con đĩ m mmmmmasdasd

---
+++ PZS.Core/Interfaces/IAccountRepository.cs
@@ -0,0 +1,13 @@
+﻿using PZS.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZS.Core.Interfaces
+{
+    public interface IAccountRepository: IGenericRepository<Account>
+    {
+    }
+}
